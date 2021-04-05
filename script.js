a20b7f8cafe33b4de8bc4f739ad66bff7bb4b5e5
dlewis401 <lewisandlloydgaming@gmail.com>
2021-04-05 17:41:24 +0100

    Rewrote the JavaScript to work with Functions & added HTML code to the HTML File

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
+function computerSelection() {
 const computerPlay = ["Rock", "Paper", "Scissors"];
-let computerSelection = computerPlay[Math.floor(Math.random()*computerPlay.length)];
+let computerChoice = computerPlay[Math.floor(Math.random()*computerPlay.length)];
 
-let computerMessages = document.querySelector('.computerMessage');
+let computerMessages = document.querySelector('.computerChose');
 
+computerMessages.textContent = computerChoice;
 
-let buttonPlayer = document.querySelector('.Rock');
-let playerMessages = document.querySelector('.playerMessage')
-buttonPlayer.addEventListener('click', function(){
-    playerMessages.textContent = "Player Chose: Rock";
-    computerMessages.textContent = 'Computer Chose: ' + computerSelection;
-});
-playerMessages.appendChild(buttonPlayer);
+return computerChoice;
 
-let buttonPlayer1 = document.querySelector('.Paper');
-let playerMessages1 = document.querySelector('.playerMessage')
-buttonPlayer1.addEventListener('click', function(){
-    playerMessages1.textContent = "Player Chose: Paper";
-    computerMessages.textContent = 'Computer Chose: ' + computerSelection;
-});
-playerMessages1.appendChild(buttonPlayer1);
+}
 
-let buttonPlayer2 = document.querySelector('.Scissors');
-let playerMessages2 = document.querySelector('.playerMessage')
-buttonPlayer2.addEventListener('click', function(){
-    playerMessages2.textContent = "Player Chose: Scissors";
-    computerMessages.textContent = 'Computer Chose: ' + computerSelection;
-});
-playerMessages2.appendChild(buttonPlayer2);
+function playRound() {
+    const computerSelect = computerSelection();
 
-// TIE MESSAGE
+    // SCORES
+    let playerScore = document.querySelector('.playerScore');
+    let playerScoreCount = parseInt(playerScore.textContent);
+    console.log(playerScoreCount);
+
+    // ROCK
+    const rockOption = document.querySelector('#rock');
+};
+
+playRound();
